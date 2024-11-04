c2464766f4c5cea7496307c8146f71a503d20c71
fi3 <email@email.com>
2024-11-04 12:13:56 +0100

    Fix share_accounter
    Share accounter relay_up and relay_down shouldn't be public since we use
    them with start.
    Share accounter should transform ShareOk extension messages from upstream
    into SubmitShareSuccess messages. In order to do that should intercept
    SubmitShareExtend on from downstream, and save them. So that when it
    receive the ShareOk message from downstream it have all the info needed
    to transform it into SubmitShareSuccess

--- src/share_accounter/mod.rs
+++ src/share_accounter/mod.rs
@@ -1,11 +1,12 @@
 mod task_manager;
 
-use std::collections::HashMap;
+use std::sync::Arc;
 
 use demand_share_accounting_ext::*;
 use parser::{PoolExtMessages, ShareAccountingMessages};
 use roles_logic_sv2::{mining_sv2::SubmitSharesSuccess, parsers::Mining};
 use task_manager::TaskManager;
+use dashmap::DashMap;
 
 use crate::shared::utils::AbortOnDrop;
 
@@ -16,27 +17,40 @@ pub async fn start(
     up_sender: tokio::sync::mpsc::Sender<PoolExtMessages<'static>>,
 ) -> AbortOnDrop {
     let task_manager = TaskManager::initialize();
+    let shares_sent_up = Arc::new(DashMap::with_capacity(100));
     let abortable = task_manager
         .safe_lock(|t| t.get_aborter())
         .unwrap()
         .unwrap();
-    let relay_up_task = relay_up(receiver, up_sender);
+    let relay_up_task = relay_up(receiver, up_sender,shares_sent_up.clone());
     TaskManager::add_relay_up(task_manager.clone(), relay_up_task)
         .await
         .expect("Task Manager failed");
-    let relay_down_task = relay_down(up_receiver, sender);
+    let relay_down_task = relay_down(up_receiver, sender,shares_sent_up.clone());
     TaskManager::add_relay_down(task_manager.clone(), relay_down_task)
         .await
         .expect("Task Manager failed");
     abortable
 }
 
-pub fn relay_up(
+struct ShareSentUp {
+    channel_id: u32,
+    sequence_number: u32,
+}
+
+fn relay_up(
     mut receiver: tokio::sync::mpsc::Receiver<Mining<'static>>,
     up_sender: tokio::sync::mpsc::Sender<PoolExtMessages<'static>>,
+    shares_sent_up: Arc<DashMap<u32, ShareSentUp>>,
 ) -> AbortOnDrop {
     let task = tokio::spawn(async move {
         while let Some(msg) = receiver.recv().await {
+            if let Mining::SubmitSharesExtended(m) = &msg {
+                shares_sent_up.insert(m.job_id, ShareSentUp{
+                    channel_id: m.channel_id,
+                    sequence_number: m.sequence_number
+                });
+            };
             let msg = PoolExtMessages::Mining(msg);
             if up_sender.send(msg).await.is_err() {
                 break;
@@ -46,29 +60,21 @@ pub fn relay_up(
     task.into()
 }
 
-pub fn relay_down(
+fn relay_down(
     mut up_receiver: tokio::sync::mpsc::Receiver<PoolExtMessages<'static>>,
     sender: tokio::sync::mpsc::Sender<Mining<'static>>,
+    shares_sent_up: Arc<DashMap<u32, ShareSentUp>>,
 ) -> AbortOnDrop {
     let task = tokio::spawn(async move {
-        let mut shares_sent_up = HashMap::new();
         while let Some(msg) = up_receiver.recv().await {
             match msg {
-                PoolExtMessages::Mining(msg) => {
-                    if let Mining::SubmitSharesExtended(m) = &msg {
-                        shares_sent_up.insert(m.job_id, m.clone());
-                    };
-                    if sender.send(msg).await.is_err() {
-                        break;
-                    }
-                }
                 PoolExtMessages::ShareAccountingMessages(msg) => {
                     if let ShareAccountingMessages::ShareOk(msg) = msg {
                         let job_id_bytes = msg.ref_job_id.to_le_bytes();
                         let job_id = u32::from_le_bytes(job_id_bytes[4..8].try_into().unwrap());
                         let share_sent_up = shares_sent_up
                             .remove(&job_id)
-                            .expect("Pool sent invalid share success");
+                            .expect("Pool sent invalid share success").1;
                         let success = Mining::SubmitSharesSuccess(SubmitSharesSuccess {
                             channel_id: share_sent_up.channel_id,
                             last_sequence_number: share_sent_up.sequence_number,
